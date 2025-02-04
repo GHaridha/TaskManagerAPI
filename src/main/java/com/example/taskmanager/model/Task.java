b08723478c958208afa6e90b7095dddb7e229fa7
GHaridha <saiharidhaganesh@gmail.com>
2025-02-04 11:28:44 +0530

    created entity and repository

---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,55 @@
+package com.example.taskmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String taskTitle;
+	
+	private String taskDescription;
+	
+	private boolean completed;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTaskTitle() {
+		return taskTitle;
+	}
+
+	public void setTaskTitle(String taskTitle) {
+		this.taskTitle = taskTitle;
+	}
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+
+	public boolean isCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/taskmanager/repository/TaskManagerRepository.java
@@ -0,0 +1,33 @@
+package com.example.taskmanager.repository;
+
+import java.util.List;
+
+import com.example.taskmanager.model.Task;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class TaskManagerRepository {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void save(Task task) {
+		entityManager.persist(task);
+	}
+	
+	public Task findById(Long id){
+		return entityManager.find(Task.class, id);
+	}
+	
+	public void remove(Long id) {
+		Task task = findById(id);
+		if(task != null) {
+			entityManager.remove(task);
+		}
+	}
+	
+	public List<Task> getAllTask(){
+		return entityManager.createQuery("Select t from Task t", Task.class).getResultList();
+	}
+}
